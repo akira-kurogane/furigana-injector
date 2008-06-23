45c758f6c44502725ff7fab12da1fe9ac77654b6
akira@yayakoshi.net <akira@yayakoshi.net@80bc7dbc-4046-0410-8831-316b8a3adfa8>
2008-06-23 03:04:17 +0000

    Final rollback of Mecab merge. MecabLib VC++ project and test routine in furiganainjector.js removed.

--- XPCOM_src/MecabLib/xpidl.generation.bat
+++
@@ -1,7 +0,0 @@
-#The path must be altered so the libIDL and glib?? dlls can be found.
-set PATH=c:\mozilla-build\moztools-180compat\bin;%PATH%
-mkdir xpidl_output
-gecko-sdk\bin\xpidl.exe -I gecko-sdk\idl -o xpidl_output\IMecabLib.xpidl -m header MecabLib.idl
-echo Use the generated MecabLib.xpidl.h to replace IMecabLib.h. N.B. There is a section for between "#if 0 ... #end if" directives which can be used to make a template for MecabLib.h and MecabLib.cpp. This should be deleted. > xpidl_output\readme.txt
-gecko-sdk\bin\xpidl.exe -I gecko-sdk\idl -o xpidl_output\MecabLib.xpidl -m typelib MecabLib.idl
-copy xpidl_output\MecabLib.xpidl.xpt "..\components\MecabLib.xpt"
